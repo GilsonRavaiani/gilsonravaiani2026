ca99a5550691cb8bb1dce8f8a7e0d524402ef943
gilsonravaiani <gravaiani@yahoo.com.br>
2025-08-31 13:14:30 -0300

    Atualizando index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota padrão para servir index.html
 app.get('/', (req, res) => {
-  res.send('🚀 Meu site está rodando no Docker!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
