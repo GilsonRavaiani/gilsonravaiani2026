ee66c410e5faf3f6f483a0ad8d079765fe0e8365
gilsonravaiani <gravaiani@yahoo.com.br>
2025-08-31 13:24:21 -0300

    Atualizando index.js linha 11

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 app.listen(PORT, () => {
