a8ac622d1a7606ae86c649661a059fae370efcb2
gilsonravaiani <gravaiani@yahoo.com.br>
2025-08-31 13:21:09 -0300

    Atualizando index.js 2

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos da pasta 'public'
+// Servir arquivos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota padrão para servir index.html
+// Rota principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
